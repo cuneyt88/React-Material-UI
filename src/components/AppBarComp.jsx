8630be864e573eaf58cf459e0375d4a159da96c7
Cüneyt Ekrem ARI <cuneyt7088@gmail.com>
2023-04-05 14:55:02 +0300

    App Bar development in progress

--- src/components/AppBarComp.jsx
+++ src/components/AppBarComp.jsx
@@ -11,7 +11,7 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
 
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
@@ -36,10 +36,10 @@ function AppBarComp() {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar position="static" color='secondary'>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <LibraryBooksIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
           <Typography
             variant="h6"
             noWrap
@@ -55,7 +55,7 @@ function AppBarComp() {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            BilgiÖlçer
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
@@ -94,7 +94,7 @@ function AppBarComp() {
               ))}
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <LibraryBooksIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
           <Typography
             variant="h5"
             noWrap
@@ -111,7 +111,7 @@ function AppBarComp() {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            BilgiÖlçer
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
@@ -128,7 +128,7 @@ function AppBarComp() {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Cüneyt ARI" src="/static/images/avatar/2.jpg" />
               </IconButton>
             </Tooltip>
             <Menu
